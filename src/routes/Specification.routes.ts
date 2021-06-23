235afcf552514c060e8193bb1eea743a4d75d9ef
Carlos Alberto Jr <carlosalbertobzjr@gmail.com>
2021-06-23 11:54:12 -0300

    [CAJR] Refactoring specification route

--- src/routes/Specification.routes.ts
+++ src/routes/Specification.routes.ts
@@ -1,27 +1,11 @@
 import { Request, Response, Router } from "express";
 
-import { Specification } from "../modules/Cars/models/Specification";
-import { SpecificationRepository } from "../modules/Cars/repositories/Specification/SpecificationRepository";
-import { CreateSpecificationService } from "../modules/Cars/services/Specification/CreateSpecification/CreateSpecificationService";
-import { FindByNameSpecificationService } from "../modules/Cars/services/Specification/FindByNameSpecification/FindByNameSpecificationService";
+import { createCategoryController } from "../modules/Cars/services/Category/CreateCategory";
 
 const specificationRoutes = Router();
-const specificationRepository = new SpecificationRepository();
 
-specificationRoutes.post("/", (req: Request, res: Response) => {
-    const { name, description } = req.body;
-    const findByNameSpecificationService = new FindByNameSpecificationService(
-        specificationRepository
-    );
-    const createSpecificationService = new CreateSpecificationService(
-        specificationRepository,
-        findByNameSpecificationService
-    );
-    const specification: Specification = createSpecificationService.execute({
-        name,
-        description,
-    });
-    res.status(201).json(specification);
-});
+specificationRoutes.post("/", (req: Request, res: Response) =>
+    createCategoryController.handler(req, res)
+);
 
 export { specificationRoutes };
