ab41e4ce6ea7499385fc982b034d0178ac3d1be4
Carlos Alberto Jr <carlosalbertobzjr@gmail.com>
2021-06-25 03:48:35 -0300

    [CAJR] Removing files from tmp dir

--- src/modules/Cars/services/Category/ImportCategory/ImportCategoryService.ts
+++ src/modules/Cars/services/Category/ImportCategory/ImportCategoryService.ts
@@ -44,6 +44,7 @@ export class ImportCategoryService {
                     categories.push({ name, description });
                 })
                 .on("end", () => {
+                    fs.promises.unlink(file.path);
                     resolve(categories);
                 })
                 .on("error", (err) => {
